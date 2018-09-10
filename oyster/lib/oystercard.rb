1510895653134cbca7a9dbd7a2a3df8821c2c268
Ruaridhi O'Scanlain <ruaridhioscanlain@MacBook-Pro-7.local>
2018-09-10 15:21:47 +0100

    Create a top up method with an argument of money

--- oyster/lib/oystercard.rb
+++ oyster/lib/oystercard.rb
@@ -6,5 +6,8 @@ class Oystercard
     @balance = 0
   end
 
+  def top_up(money)
+
+  end
 
 end
--- oyster/spec/oystercard_spec.rb
+++ oyster/spec/oystercard_spec.rb
@@ -8,4 +8,10 @@ describe Oystercard do
   it "the balance should be 0 by default" do
     expect(subject.balance).to eq 0
   end
+
+  it "should have a top up method" do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+
 end
