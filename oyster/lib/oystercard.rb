f05b35ec5f5d0d0b852042934d87a7de74232546
Ruaridhi O'Scanlain <ruaridhioscanlain@MacBook-Pro-7.local>
2018-09-11 17:26:08 +0100

    added touch in/out functionality + in journey

--- oyster/lib/oystercard.rb
+++ oyster/lib/oystercard.rb
@@ -1,10 +1,12 @@
 class Oystercard
 
   attr_reader :balance
+  #attr_accessor :in_journey
   MAXIMUM = 90
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(money)
@@ -16,4 +18,15 @@ class Oystercard
     @balance -= money
   end
 
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
+  end
 end
--- oyster/spec/oystercard_spec.rb
+++ oyster/spec/oystercard_spec.rb
@@ -6,7 +6,6 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
-
   it "when we top up £10, add to balance" do
     subject.top_up(10)
     expect(subject.balance).to eq 10
@@ -21,5 +20,32 @@ describe Oystercard do
 
   it "should deduct money from balance when used" do
     expect {subject.deduct(1)}.to change{subject.balance}.by(-1)
+  end
+
+describe "#touch_in" do
+  it "should have a touch in method" do
+    expect(subject).to respond_to(:touch_in)
+  end
+
+  it 'oystercard is in use' do
+    expect{subject.touch_in}.to change{subject.in_journey?}.to true
+  end
+end
+
+describe "in_journey"
+  it "it should have a in journey method" do
+    expect(subject).to respond_to(:in_journey?)
+  end
+
+  it "it is not in use" do
+    expect(subject.in_journey?).to eq(false)
+    # expect(subject).not_to be_in_journey
+  end
+
+
+
+  it 'not in use after touch out'do
+    subject.touch_in
+    expect{subject.touch_out}.to change{subject.in_journey?}.to false
   end
 end
